1261476a222f4929f8a8a8a7ee26e2f8df0b5fd0
ahmed10veer <ahmed10veer@gmail.com>
2014-08-26 16:51:13 +0600

    photo app over image text

--- application/views/applications/photography/home.php
+++ application/views/applications/photography/home.php
@@ -17,6 +17,29 @@
     background-repeat: no-repeat;
     
 }
+.overimgtext2
+{
+    position: absolute;
+    bottom: 5%; 
+    /*border: 2px solid #75B3E6;*/
+    background:rgba(100,100,111,0.6); 
+    width: 97%;
+    color: white;
+    font-size: larger; 
+    text-align: center;
+    padding: 12px;
+}
+.overimgtext
+{
+    position: absolute;
+    top: 85%; 
+    background:rgba(100,100,111,0.6); 
+    width: 100%;
+    color: white;
+    font-size: larger; 
+    text-align: center;
+    padding: 12px;
+}
 .carousel {
 width:100%; 
 margin:0 auto; /* center your carousel if other than 100% */ 
@@ -25,35 +48,40 @@ margin:0 auto; /* center your carousel if other than 100% */
 #imgbuttonf{ height: 32px; width: 32px; position: absolute; right: 32px; top: 30px;}
 </style>
 
-
-
 <div class="row col-md-12">
-        <div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
-            <div class="carousel-inner">
-                <?php if(!empty($image_list)): ?>
-                    <?php $i=1; ?>
-                    <?php foreach ($image_list as $image_info): ?>
-                        <div class="item <?php echo ($i == 1) ? 'active' : ''; ?>">
-                            <div  id="div_slider" class="slider-size" style="background-image: url('<?php echo base_url().PHOTOGRAPHY_IMAGE_PATH.$image_info['img'];?>');" >
-                               
+    <div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
+        <div class="carousel-inner">
+            <?php if (!empty($image_list)): ?>
+                <?php $i = 1; ?>
+                <?php foreach ($image_list as $image_info): ?>
+                    <div class="item <?php echo ($i == 1) ? 'active' : ''; ?>">
+                        <div  id="div_slider" class="slider-size" style="background-image: url('<?php echo base_url() . PHOTOGRAPHY_IMAGE_PATH . $image_info['img']; ?>');" >
+                            <div class="col-md-12 col-sm-12 col-xs-12 overimgtext">
+                                <div class="col-md-4 col-sm-4 col-xs-4">
+                                    <span>top text</br>bottom text</span>
+                                </div>
+                                <div class="col-md-4 col-sm-4 col-xs-4">
+                                    <span>top text</br>bottom text</span>
+                                </div>
+                                <div class="col-md-4 col-sm-4 col-xs-4">
+                                    <span>top text</br>bottom text</span>
+                                </div>
                             </div>
                         </div>
-                        <?php $i++; ?>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-            </div> 
-            <a href="#myCarousel" data-slide="next">
-                <!--<span class="glyphicon glyphicon-chevron-left"></span>-->
-                <img id="imgbuttonf" src="<?php echo base_url(); ?>resources/images/frontArrow.png" onclick=""/>
-            </a>
-            <a  href="#myCarousel" data-slide="prev">
-                <!--<span class="glyphicon glyphicon-chevron-right"></span>-->
-                <img id="imgbuttonb" src="<?php echo base_url(); ?>resources/images/backArrow.png" onclick=""/>
-            </a>
-        
-        
-        </div>
-    
+                    </div>
+                    <?php $i++; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div> 
+        <a href="#myCarousel" data-slide="next">
+            <!--<span class="glyphicon glyphicon-chevron-left"></span>-->
+            <img id="imgbuttonf" src="<?php echo base_url(); ?>resources/images/frontArrow.png" onclick=""/>
+        </a>
+        <a  href="#myCarousel" data-slide="prev">
+            <!--<span class="glyphicon glyphicon-chevron-right"></span>-->
+            <img id="imgbuttonb" src="<?php echo base_url(); ?>resources/images/backArrow.png" onclick=""/>
+        </a>
+    </div>
 </div>
 
 <script type="text/javascript">
