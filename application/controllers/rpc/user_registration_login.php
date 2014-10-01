55d40afba8b085b50f9c868d68be7b4e028f5dec
nazmulhasan <nazmul@apurbatech.com>
2014-10-01 13:37:05 +0600

    user info is added at registration

--- application/controllers/rpc/user_registration_login.php
+++ application/controllers/rpc/user_registration_login.php
@@ -14,27 +14,27 @@ class User_registration_login extends JsonRPCServer {
     function __construct() {
         parent::__construct();
         $this->load->library('ion_auth');
-        $this->load->model("dataprovider_model");
-        $this->load->library("basic_profile");
-        $this->load->library("org/interest/special_interest");
-        $this->load->library('form_validation');
-        $this->load->helper('url');
-        $this->load->library('org/question/security_question_library');
-        $this->load->library("org/rpc/user_rpc_library");
-        $this->load->library("profile");
-        $this->load->library('org/utility/Utils');
+        //$this->load->model("dataprovider_model");
+        //$this->load->library("basic_profile");
+        //$this->load->library("org/interest/special_interest");
+        //$this->load->library('form_validation');
+        //$this->load->helper('url');
+        //$this->load->library('org/question/security_question_library');
+        //$this->load->library("org/rpc/user_rpc_library");
+        //$this->load->library("profile");
+        //$this->load->library('org/utility/Utils');
         // Load MongoDB library instead of native db driver if required
         $this->config->item('use_mongodb', 'ion_auth') ?
                         $this->load->library('mongo_db') :
                         $this->load->database();
 
-        $this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+        //$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
     }
 
     function index() {
         
     }
-
+    
     function getUserInfo() {
         
     }
