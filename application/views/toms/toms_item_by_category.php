41661c46647dea4c4e7ebac57109012494b749a7
ahmed10veer <ahmed10veer@gmail.com>
2014-10-22 12:47:59 +0600

    toms: moved designs to css

--- application/views/toms/toms_item_by_category.php
+++ application/views/toms/toms_item_by_category.php
@@ -1,76 +1,16 @@
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>resources/bootstrap3/css/blog_app.css" />
-<div id="testnavmenu">
-    <div id='mega_nav'>
-        <ul>
-            <li><a href='<?php echo base_url()?>test/run_toms_page'><span>HOME</span></a></li>
-            <li onmouseout="document.getElementById('tray_women').style.display = 'none';" onmouseover="document.getElementById('tray_women').style.display = 'block';" ><a href='#'>WOMEN</a></li>
-            <li onmouseout="document.getElementById('tray_men').style.display = 'none';" onmouseover="document.getElementById('tray_men').style.display = 'block';" ><a href='#'>MEN</a></li>
-            <li onmouseout="document.getElementById('tray_kids').style.display = 'none';" onmouseover="document.getElementById('tray_kids').style.display = 'block';" ><a href='#'>KIDS</a></li>
-            <li><a href='#'>STORIES</a></li>
-            <li><a href='#'>ONE FOR ONE</a></li>
-        </ul>
-    </div>
-</div>
+<?php $this->load->view("toms/topnav"); ?>
 
-<div style="position: relative">
-    <div class="mega_nav_tray container-fluid"  id="tray_women"  onmouseout="document.getElementById('tray_women').style.display = 'none';" onmouseover="document.getElementById('tray_women').style.display = 'block';" >
-        <div class="row">
-            <div id="white_back">
-                <div class="col-md-2 tray_featured" style="border: 1px solid black">
-                    <a href='<?php echo base_url() ?>test/run_toms_page'>View All</a>
-                    <a href='#'>New Arrival</a>
-                    <a href='#'>Gift Card</a>
-                </div>
-                <div class="col-md-10">
-                    <div class="col-md-2">
-                        <div class="item_card">
-                            <div class="col-md-12" >
-                                <img class="img-responsive" src="<?php echo base_url() ?>resources/images/video.jpg" />
-                            </div>
-                            <div class="col-md-12" >
-                                BEST SELLERS
-                                BEST SELLERS
-                                BEST SELLERS
-                                BEST SELLERS
-                                <span>BEST SELLERS</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div id="brw_back">
-            <div class="row">
-                <div class="col-md-2">1 asdasd</div>
-                <div class="col-md-2">2 sacas2</div>
-                <div class="col-md-2">3 sadcsdf</div>
-                <div class="col-md-2">4 sddvdvsd</div>
-                <div class="col-md-2">5 fvdfbdf</div>
-                <div class="col-md-2">6 sdfvfv</div>
-            </div>
-        </div>
-    </div>
-    
-    <div class="mega_nav_tray container-fluid"  id="tray_men" onmouseout="document.getElementById('tray_men').style.display = 'none';" onmouseover="document.getElementById('tray_men').style.display = 'block';" >
-        <div class="row">
-            <div>
-                <div class="col-md-2">success uhasdhoasd </div>
-                <div class="col-md-10">bog colums</div>
-            </div>
-        </div>
-
-        <div style="height: 50px; background: red"></div>
-    </div>
-</div>
-
-<div class="row col-md-12">
+<div class="row">
     
     <!--VERTI MENU-->
     <div id="left_menu" class="col-md-3 pull-left">
         
         <!--MENU BOXES-->
         <div class="menubox">
-            <div class="expandable">first</div>            
+            <div class="expandable">
+                first
+                <div class="plus_minus pull-right">+</div>
+            </div>            
             <div class="collupsible">
                 <div>first item </div>
                 <div>first item </div>
@@ -81,7 +21,10 @@
         </div>
         
         <div class="menubox">
-            <div class="expandable">Second</div>            
+            <div class="expandable">
+                Second
+                <div class="plus_minus pull-right">+</div>
+            </div>            
             <div class="collupsible">
                 <div>first item </div>
                 <div>first item </div>
@@ -92,7 +35,10 @@
         </div>
         <div>
             <div class="menubox">
-                <div class="expandable">inner</div>            
+                <div class="expandable">
+                    inner
+                    <div class="plus_minus pull-right">+</div>
+                </div>            
                 <div class="collupsible">
                     <div>first item </div>
                     <div>first item </div>
@@ -102,7 +48,10 @@
                 </div>
             </div>
             <div class="menubox">
-                <div class="expandable">inner</div>            
+                <div class="expandable">
+                    inner
+                    <div class="plus_minus pull-right">+</div>                
+                </div>            
                 <div class="collupsible">
                     <div>first item </div>
                     <div>first item </div>
@@ -112,7 +61,10 @@
                 </div>
             </div>
             <div class="menubox">
-                <div class="expandable">inner</div>            
+                <div class="expandable">
+                    inner
+                    <div class="plus_minus pull-right">+</div>
+                </div>            
                 <div class="collupsible">
                     <div>first item </div>
                     <div>first item </div>
@@ -123,10 +75,6 @@
             </div>
         </div>
     </div>
-    
-    
-    
-    
     
     
     <!--ITEMS-->
@@ -212,315 +160,28 @@
                 </div>
             </div>
         </div>
-
     </div>
 </div>
 
-
-
-<style>
-    #mega_nav ul,
-    #mega_nav li,
-    /*#mega_nav span,*/
-    #mega_nav a {
-      margin: 0;
-      padding: 0;
-      position: relative;
-    }
-    #mega_nav {
-      background: #5A534C;
-    }
-    #mega_nav:after,
-    #mega_nav ul:after {
-      content: '';
-      display: block;
-      clear: both;
-    }
-    #mega_nav > ul > li > a {
-      /*background: #5A534C;*/
-      color: #ffffff;
-      display: block;
-      font-weight: bolder;
-      font-family: Helvetica, Arial, Verdana, sans-serif;
-      padding: 19px 20px;
-      text-decoration: none;
-    }
-    #mega_nav ul {
-      list-style: none;
-    }
-    #mega_nav > ul > li {
-      /*display: inline-block;*/
-      float: left;
-      /*margin: 0;*/
-    }
-    #mega_nav > ul > li > a {
-      color: #ffffff;
-      font-size: 12px;
-    }
-    #mega_nav > ul > li:hover:after {
-      content: '';
-      display: block;
-      width: 0;
-      height: 0;
-      position: absolute;
-      left: 50%;
-      bottom: 0;
-      border-left: 10px solid transparent;
-      border-right: 10px solid transparent;
-      border-bottom: 10px solid white;
-      margin-left: -10px;
-    }    
-    #mega_nav > ul > li.active > a,
-    #mega_nav > ul > li:hover > a {
-      color: greenyellow;
-    }
-    .mega_nav_tray
-    {
-        width: 888px;
-        padding: 20px;
-        border-radius: 2px;
-        box-shadow: 0 0 3px #000000;
-        -moz-box-shadow: inset 0 0 3px #000000;
-        -webkit-box-shadow: inset 0 0 3px #000000;
-        display: none;
-        position: absolute;
-        top: 100%;
-        left: 0;
-        z-index: 99;
-        background: #ffffff;
-    }
-    li
-    {
-        list-style: none;
-    }
-    #brw_back
-    {
-        padding: 40px;
-        background-color: #f8f5ee;
-    }
-    .tray_featured a
-    {
-        display: block;
-        font-weight: bold;
-        color: #5A534C;
-        text-align: left;
-        padding: 4px;
-/*        text-decoration: none;
-        text-transform: uppercase;*/
-        
-    }
-    .page_section_heading
-    {
-        font-size: 26px;
-        line-height: 1;
-        text-transform: uppercase;
-        color: #5A534C;
-        text-align: center;
-        font-weight: bolder;
-        padding: 16px;
-        margin-top: 10px;
-    }
-    
-    
-    #carousel-bestsellers .carousel-inner
-    {
-        margin-bottom:50px;
-    }
-    #carousel-bestsellers .carousel-indicators
-    {
-        bottom:-50px;
-    }
-    #carousel-bestsellers .carousel-indicators li
-    {
-        background-color: lightgray;
-        bottom:-50px;
-    }
-    #carousel-bestsellers .carousel-indicators .active
-    {
-        background-color: lightskyblue;
-    }
-    #carousel-bestsellers .carousel-control
-    {
-        background-image: none;
-        color: gray;
-        top: 100%;
-        bottom:-40px;
-        width: 44px;
-        z-index: 20;
-    }
-    #carousel-bestsellers .left.carousel-control{
-        left: 30%
-    }
-    #carousel-bestsellers .right.carousel-control{
-        right: 30%
-    }
-    .item_card
-    {
-        font-size: 16px;
-        text-align: center;
-        color: #5A534C;
-    }
-    .item_card span
-    {
-        display: block;
-        color: limegreen;
-    }
-    .item_card img
-    {
-        margin: 8px;
-        max-height: 150px;
-        max-width: 150px;
-    }
-    .mega_nav_tray .item_card
-    {
-        font-size: 13px;
-    }
-    .mega_nav_tray .item_card img
-    {
-        margin: 8px;
-        max-height: 150px;
-        max-width: 150px;
-    }
-</style>
-
 <script>
-
-    function confirmation_vote(league_id)
-    {
-        $("#league_id").val(league_id);
-        $("#confirmModal").modal("show");
-    }
-    function post_vote()
-    {
-        var league_id = $("#league_id").val();
-        $.ajax({
-                type: 'POST',
-                url: '<?php echo base_url(); ?>' + "asdasd/application/xstreambanter/post_vote",
-                dataType: 'json',
-                data: {
-                    league_id: league_id
-                },
-                success: function(data) {
-                    location.reload();
-                }
-            });
-    }
-</script>
-<style>
-    .blue_banner
-    {
-        color: white;
-        background-color:#3F48CC;
-    }
-    .title{
-        font-size: 20px;
-        text-align: center;
-    }
-    .heading{
-        padding: 15px;
-        font-size: 25px;
-        text-align: center;
-    }
-    .lr_image
-    {
-        height: 18px;
-        padding: 4px 0px 0px;
-    }
-</style>
-
-
-
-<!--<div class="col-md-6">
-    <?php echo 'echo error: ';?>
-    <?php echo $error;?>
-    <?php var_dump($error);?>
-
-    <?php echo form_open_multipart('test/upload_crop');?>
-
-        <input type="file" name="userfile" size="20" />
-        <br /><br />
-        <input type="submit" value="upload" />
-
-    <?php echo form_close();?>
-        
-        
-        
-</div>-->
-<style>
-    #left_menu
-    {
-        /*box-sizing: border-box;*/
-        /*background-color: black;*/
-        min-height: 200px;
-        clear: both;
-        color: #53504c;
-        font-size: 16px;
-    }
-    #left_menu a:hover
-    {
-        text-decoration: none;
-    }
-    #left_menu a
-    {
-        display: block;
-        color: #53504c;
-        font-size: 16px;
-    }
-    #left_menu > div
-    {
-        margin-top: 15px;
-    }
-    .menubox
-    {
-        font-size: 16px;
-        color: #53504c;
-        margin-bottom: -2px;
-        border: 2px solid #E9E4DE;
-        background-color: #F6F3EF;
-    }
-    
-    
-    
-    .collupsible a:hover,
-    .collupsible > div:hover
-    {
-        color: #57b9e1;
-        background-color: #E9E4DE;
-    }
-    .collupsible > div
-    {
-        display: block;
-        padding: 5px;
-        padding-left: 20px;
-        font-size: 16px;
-    }
-    .collupsible
-    {
-        padding-bottom: 10px;
-    }
-    .expandable
-    {
-        padding: 15px;
-        color: #53504c;
-        font-size: 18px;
-        font-weight: bolder;
-        text-transform: uppercase;
-    }
-/*    .menubox a, .menubox_item
-    {
-        color: #53504c;
-        display: block;
-        font-size: 16px;
-        font-weight: normal;
-    }*/
-</style>
-<script>
-        $('.expandable').click(function(){
-            var collup_var = $(this).siblings('.collupsible:first');
-                collup_var.slideToggle();
-                if (collup_var.is(":visible")) {
-                    alert('Element is visible: ' + collup_var.toString());
-                }
+    $('.expandable').each(function(){
+        if ($(this).siblings('.collupsible:first').is(":visible")) {
+            $(this).children(".plus_minus").text("--");
+        }
+        else {
+            $(this).children(".plus_minus").text("+");
+        }
+    });
+    $('.expandable').click(function(){
+        var collup_var = $(this).siblings('.collupsible:first');
+            collup_var.toggle();
+            if (collup_var.is(":visible")) {
+                $(this).children(".plus_minus").text("--");
             }
-        );
+            else {
+                $(this).children(".plus_minus").text("+");
+            }
+//                $(this).siblings('.collupsible:first').slideToggle();
+        }
+    );
 </script>
