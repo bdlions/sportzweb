fb2aa95620fadd4c82a2466c90a5cdc719cd842d
nazmulhasan <nazmul@apurbatech.com>
2014-10-29 15:11:03 +0600

    rpc news app

--- application/controllers/rpc/app_news.php
+++ application/controllers/rpc/app_news.php
@@ -61,24 +61,45 @@ class App_news extends JsonRPCServer {
         $response['news_list'] = $news_list;
         return json_encode($response);
     }
-    
-    function get_news_list($news_category_id, $news_subcategory_id)
+    /*
+     * This method will return news list at home page 
+     * @Author Nazmul on 29th October 2014
+     */
+    function get_home_page_news_list()
     {
         $response = array();
-        $news_subcategory_list = array();
         $news_list = array();
-        $news_configuration = array();
-        $news_subcategory_list_array = array();
-        if($news_category_id > 0)
+        $news_home_page_configuration = $this->news_app_library->get_news_home_page_configuration();
+        if(array_key_exists('news_id_news_info_map', $news_home_page_configuration))
         {
-            $news_configuration = $this->news_app_library->get_news_category_configuration($news_category_id);            
-            $news_subcategory_list_array = $this->news_app_library->get_all_news_sub_category($news_category_id)->result_array();
-        
+            $news_list_array = $news_home_page_configuration['news_id_news_info_map'];
+            foreach($news_list_array as $news_info)
+            {
+                $news = array(
+                    'id' => $news_info['news_id'],
+                    'news_id' => $news_info['news_id'],
+                    'headline' => $news_info['headline'],
+                    'picture' => $news_info['picture']
+                );
+                $news_list[] = $news;
+            }
         }
-        else if($news_subcategory_id > 0)
-        {
-            $news_configuration = $this->news_app_library->get_news_sub_category_configuration($news_subcategory_id);
-        }
+        $response['news_list'] = $news_list;
+        return json_encode($response);
+    }
+    
+    /*
+     * This method will retunr news list of a category and also news list of each subcateogyr of that category
+     * @param $news_category_id, news category id
+     * @Author Nazmul on 29October 2014
+     */
+    function get_news_list($news_category_id)
+    {
+        $result = array();
+        $category_news_list = array();
+        $subcategory_id_news_list = array();
+        $subcategory_news_list = array();
+        $news_configuration = $this->news_app_library->get_news_category_configuration($news_category_id);            
         if(!empty($news_configuration))
         {
             if(array_key_exists('news_id_news_info_map', $news_configuration))
@@ -92,21 +113,49 @@ class App_news extends JsonRPCServer {
                         'headline' => $news_info['headline'],
                         'picture' => $news_info['picture']
                     );
-                    $news_list[] = $news;
+                    $category_news_list[] = $news;
                 }
             }
         }
+        $news_subcategory_list_array = $this->news_app_library->get_all_news_sub_category($news_category_id)->result_array();
         foreach($news_subcategory_list_array as $subcategory_info)
         {
-            $subcategory = array(
-                'id' => $subcategory_info['id'],
-                'title' => $subcategory_info['title']
-            );
-            $news_subcategory_list[] = $subcategory;
+            $subcategory_news_list = array();
+            $news_configuration = $this->news_app_library->get_news_sub_category_configuration($subcategory_info['id']);
+            if(!empty($news_configuration))
+            {
+                if(array_key_exists('news_id_news_info_map', $news_configuration))
+                {
+                    $news_list_array = $news_configuration['news_id_news_info_map'];
+                    foreach($news_list_array as $news_info)
+                    {
+                        $news = array(
+                            'id' => $news_info['news_id'],
+                            'news_id' => $news_info['news_id'],
+                            'headline' => $news_info['headline'],
+                            'picture' => $news_info['picture']
+                        );
+                        $subcategory_news_list[] = $news;
+                    }
+                    $subcategory_id_news_list[$subcategory_info['id']] = $subcategory_news_list;
+                }
+            }
         }
-        $response['news_subcategory_list'] = $news_subcategory_list;
-        $response['news_list'] = $news_list;        
-        return json_encode($response);
-        
+        $result['category_news_list'] = $category_news_list;
+        $result['subcategory_id_news_list'] = $subcategory_id_news_list;
+        return json_encode($result);
+    }
+    
+    /*
+     * This method will return news info
+     * @param $news_id, news id
+     * @Author Nazmul on 29th October 2014
+     */
+    function get_news_info($news_id)
+    {
+        $result = array();
+        $news_info = $this->news_app_library->get_news_info($news_id);
+        $result['news_info'] = $news_info;
+        return json_encode($result);
     }
 }
