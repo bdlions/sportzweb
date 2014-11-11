9a871a5112cba9aa05d4351f7e5faef24badfd2f
nazmulhasan <nazmul@apurbatech.com>
2014-11-11 13:05:59 +0600

    rpc to return member profile

--- application/controllers/rpc/member_profile.php
+++ application/controllers/rpc/member_profile.php
@@ -13,7 +13,9 @@ class Member_profile extends CI_Controller{
         parent::__construct();
         $this->load->library('ion_auth');
         $this->load->library('basic_profile');
+        $this->load->library("follower");
         $this->load->library("org/interest/special_interest");
+        $this->load->library("statuses");
         // Load MongoDB library instead of native db driver if required
         $this->config->item('use_mongodb', 'ion_auth') ?
                         $this->load->library('mongo_db') :
@@ -22,6 +24,35 @@ class Member_profile extends CI_Controller{
 
     function index() {
         
+    }
+    
+    /*
+     * This method will return user profile info
+     * @param $user_id, user id of a profile
+     * @param $is_myself, 1 or 0 based on user id is mine or not
+     * @Author Nazmul on 11th November 2014
+     */
+    function show($user_id, $is_myself)
+    {
+        $response = array();
+        $response['profile_type'] = PROFILE_NON_FOLLOWER;
+        if($is_myself == 1)
+        {
+            $response['profile_type'] = PROFILE_MYSELF;
+        }
+        else
+        {
+            $response = array_merge($response, $this->follower->get_relation_with_user($user_id));     
+        }
+        $response['newsfeeds'] = $this->statuses->get_statuses(STATUS_LIST_USER_PROFILE, $user_id);
+        $response['status_list_id'] = STATUS_LIST_USER_PROFILE;
+        $response['mapping_id'] = $user_id;
+        $response['user_info'] = $this->ion_auth->get_user_info($user_id);
+        
+        $response['status_or_comment_in'] = STATUS_POSTED_IN_BASIC_PROFILE;
+        $response['basic_profile'] = $this->basic_profile->get_profile_info($user_id);
+        echo json_encode($response);
+        //return json_encode($response);
     }
     
     /*
