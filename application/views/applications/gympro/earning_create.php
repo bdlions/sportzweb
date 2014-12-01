f35b55e7b828e475df4f7c902ea5754e1b2d782a
ahmed10veer <ahmed10veer@gmail.com>
2014-12-01 19:09:57 +0600

    earnings, page 1

--- application/views/applications/gympro/earning_create.php
+++ application/views/applications/gympro/earning_create.php
@@ -1,3 +1,14 @@
+<script>
+    $(function () {
+        $("#datepicker").datepicker({
+            showOn: "button",
+            buttonImage: "<?php echo base_url();?>resources/images/calendar.png",
+            buttonImageOnly: true,
+            buttonText: "Select date"
+        });
+    });
+</script>
+
 <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>resources/bootstrap3/css/gympro.css">
 
 <div class="container-fluid">
@@ -8,11 +19,22 @@
         <div class="col-md-10">
             
             
-                <div class="row">
+            <div class="row">
+                <div class="col-md-12">
+                    Adding new session
+                    <div class="row form-group">
+                        <div class="col-md-4" style="border-bottom: 1px solid #999999">
+
+                        </div>
+
+                    </div>
+                </div>
+
+                    
                     <div class="col-md-6">
                         <div class="row form-group">
                             <div class="col-md-2 control-div">Title: </div>
-                            <div class="col-md-6">
+                            <div class="col-md-9">
                                 <input class="form-control">
                             </div>
                         </div>
@@ -39,7 +61,7 @@
                         <div class="row form-group">
                             <div class="col-md-2 control-div">Date:</div>
                             <div class="col-md-6">
-                                <input class="form-control">
+                                <input class="" id="datepicker" >
                             </div>
                         </div>
                         <div class="row form-group">
@@ -62,7 +84,7 @@
                         </div>
                         <div class="row form-group">
                             <div class="col-md-2 control-div">Location</div>
-                            <div class="col-md-6">
+                            <div class="col-md-9">
                                 <input class="form-control"> 
                             </div>
                         </div>
@@ -96,51 +118,12 @@
                                 </select>
                             </div>
                         </div>
-                        <div class="row form-group">
-                            <div class="col-md-2 control-div">Clint:</div>
-                            <div class="col-md-4">
-                                <select class="form-control">
-                                    <option class="form-control">
-                                        Clint
-                                    </option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="row form-group">
-                            <div class="col-md-2 control-div">Start:</div>
-                            <div class="col-md-4">
-                                <select class="form-control">
-                                    <option class="form-control">
-                                        start
-                                    </option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="row form-group">
-                            <div class="col-md-2 control-div">Finish:</div>
-                            <div class="col-md-4">
-                                <select class="form-control">
-                                    <option class="form-control">
-                                        finish
-                                    </option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="row form-group">
-                            <div class="col-md-2 control-div">Type:</div>
-                            <div class="col-md-4">
-                                <select class="form-control">
-                                    <option class="form-control">
-                                        type
-                                    </option>
-                                </select>
-                            </div>
-                        </div>
+                        
                     </div>
                     <div class="col-md-4">
                         <div class="row form-group">
                             <div class="col-md-2 control-div">Notes</div> 
-                            <div class="col-md-9">
+                            <div class="col-md-10">
                                 <textarea class="form-control" rows="10"></textarea>
                             </div>
                         </div>
