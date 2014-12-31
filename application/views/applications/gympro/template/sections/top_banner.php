814842b027ab049b6f58147380ce892a92d219e9
Rashida <rashidacse@gmail.com>
2014-12-31 16:45:20 +0600

    gympro banner

--- application/views/applications/gympro/template/sections/top_banner.php
+++ application/views/applications/gympro/template/sections/top_banner.php
@@ -1,7 +1,12 @@
 <div style="background-color: #880015; border-bottom: 12px solid #E4E4E4">
-    <div class="col-md-offset-2">
-        <a href="<?php echo base_url() ?>applications/gympro">
-            <img src="<?php echo base_url() ?>resources/images/gympro.png" style="padding-left: 60px;">
-        </a>
+    <div class="row">
+        <div class="col-md-offset-2 col-md-2">
+            <a href="<?php echo base_url() ?>applications/gympro">
+                <img src="<?php echo base_url() ?>resources/images/gympro_logo.jpg" style="height: 50px">
+            </a> 
+        </div>
+        <div class="col-md-8">
+            <img src="<?php echo base_url() ?>resources/images/gympro_banner_text.png">  
+        </div>
     </div>
 </div>
