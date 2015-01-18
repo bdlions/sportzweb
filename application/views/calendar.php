9be687605d768b6e8312861bfe83017568b31f54
Alamgir Kabir <alamgirkabir7@gmail.com>
2015-01-18 16:45:39 +0600

    add views and day view swithcher
    add various views(day, month, week) and click on a day cell open it as a
    day view

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -7,10 +7,24 @@
     $(document).ready(function() {
 
         $('#calendar').fullCalendar({
+            header: {
+                left: 'prev,next',
+                center: 'title',
+                right: 'month,basicWeek,basicDay'
+            },
             defaultDate: '2014-11-12',
             editable: true,
             eventLimit: true, // allow "more" link when too many events
-            events: JSON.parse('<?php echo $events?>')
+            events: JSON.parse('<?php echo $events?>'),
+            eventClick:  function(event, jsEvent, view) {
+                $('#modalTitle').html(event.title);
+                $('#modalBody').html(event.description);
+                $('#fullCalModal').modal();
+            },
+            dayClick: function(date, jsEvent, view) {
+                $('#calendar').fullCalendar('gotoDate', date);
+                $('#calendar').fullCalendar('changeView', 'agendaDay');
+            }
         });
 
     });
@@ -18,3 +32,20 @@
 </script>
 
 <div id='calendar'></div>
+<div id="fullCalModal" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span> <span class="sr-only">close</span></button>
+                <h4 id="modalTitle" class="modal-title"></h4>
+            </div>
+            <div id="modalBody" class="modal-body">
+                <a href="#">Edit</a>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success">Save</button>
+                <button class="btn btn-default">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
