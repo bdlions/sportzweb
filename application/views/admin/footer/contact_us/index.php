b76e1d267340098f77a1c517d7a74d70afc88332
nazmulhasan <nazmul@apurbatech.com>
2015-01-24 20:40:08 +0600

    admin panel home page of contact us is updated

--- application/views/admin/footer/contact_us/index.php
+++ application/views/admin/footer/contact_us/index.php
@@ -1,31 +1,23 @@
-<script type="text/javascript">
-</script>  
-<script type="text/x-tmpl" id="tmpl_topic_list">
-  
-</script>
-                            
 <div class="panel panel-default">
     <div class="panel-heading">Customer Feedback</div>
     <div class="panel-body">
         <div class="row col-md-12">
             <div class="row form-group">
-                <div class ="col-md-2" style="padding-left: 25px;">
+                <div class ="col-md-3">
                     <a href="<?php echo base_url();?>admin/contact_us/manage_topic">
                         <button style="width:100%" id="button_create_topic_name" class="btn button-custom pull-left" >
                             Topics
                         </button>
                     </a>
                 </div>
-                <div class="col-md-2"></div>
-                <div class =" col-md-2">
+                <div class ="col-md-3">
                     <a href="<?php echo base_url().'admin/contact_us/manage_os' ?>">
                         <button style="width:100%" id="button_create_os_name" value="" class="btn button-custom " style="margin-left: 0px;">
                             Operating Systems
                         </button>
                     </a>
                 </div>
-                <div class="col-md-2"></div>
-                <div class =" col-md-2">
+                <div class ="col-md-3">
                     <a href="<?php echo base_url().'admin/contact_us/manage_browser' ?>">
                         <button style="width:100%" id="button_create_browser_name" value="" class="btn button-custom pull-right " style="margin-left: 0px;">
                             Browsers
@@ -34,14 +26,14 @@
                 </div>
             </div>
             <div class="row form-group">
-                <div class ="col-md-2" style="padding-left: 25px;">
+                <div class ="col-md-3">
                     <a href="<?php echo base_url();?>admin/contact_us/member_feedback">
                         <button style="width:100%" id="button_create_topic_name" class="btn button-custom pull-left" >
                             Member Feedback
                         </button>
                     </a>
                 </div>
-                <div class =" col-md-2">
+                <div class =" col-md-3">
                     <a href="<?php echo base_url().'admin/contact_us/non_member_feedback' ?>">
                         <button style="width:100%" id="button_create_os_name" value="" class="btn button-custom " style="margin-left: 0px;">
                             Non Member Feedback
@@ -49,8 +41,10 @@
                     </a>
                 </div>
             </div>
-            <div class="btn-group" style="padding-left: 10px;">
-                <input type="button" style="width:120px;" value="Back" id="back_button" onclick="javascript:history.back();" class="form-control btn button-custom">
+            <div class="row form-group">
+                <div class ="col-md-3">
+                    <input type="button" value="Back" id="back_button" onclick="javascript:history.back();" class="form-control btn button-custom">
+                </div>
             </div>
         </div>        
     </div>
