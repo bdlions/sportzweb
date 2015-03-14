3bf569ceff15ba0cda972694f10244711554949c
ahmed10veer <ahmed10veer@gmail.com>
2015-03-14 19:18:02 +0600

    properties in tags removed

--- application/views/common/ckeditor_customization.php
+++ application/views/common/ckeditor_customization.php
@@ -7,9 +7,25 @@
      */
     function filter_html_tags(source_tags){
         var regex = '<(?!\/?(p|a)\s*.*(?=>|\s.*>))\/?.*?>';
-//        var regex = '<(?!\/?(p)|(a)(?=>|\s.*>))\/?.*?>';
         regex = new RegExp(regex, 'gi');
         source_tags = source_tags.replace( regex, '' );
+        
+        regex = '<p\s*[^>]*>';
+        regex = new RegExp(regex, 'gi');
+        source_tags = source_tags.replace( regex, '<p>' );
+        
+        regex = '<\/p\s*[^>]*>';
+        regex = new RegExp(regex, 'gi');
+        source_tags = source_tags.replace( regex, '</p>' );
+        
+        regex = '<a\s*[^>]*>';
+        regex = new RegExp(regex, 'gi');
+        source_tags = source_tags.replace( regex, '<a>' );
+        
+        regex = '<\/a\s*[^>]*>';
+        regex = new RegExp(regex, 'gi');
+        source_tags = source_tags.replace( regex, '</a>' );
+        
         return source_tags;
     }
 
