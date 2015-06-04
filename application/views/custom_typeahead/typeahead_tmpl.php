13286f7b80f76d8c33edf64f289f3c3221d8ee5a
Redoy <nazmulislamredoy@gmail.com>
2015-06-04 20:22:41 +0600

    search bar css bug fixed

--- application/views/custom_typeahead/typeahead_tmpl.php
+++ application/views/custom_typeahead/typeahead_tmpl.php
@@ -1,162 +1,162 @@
 
-            <div class="row" id="page_late_id" style="display: none; position: absolute; z-index: 1001;">;
-                <div class="col-md-offset-2 col-md-10">
-                    <div class="pagelet">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div id="users_temp">
-                                    <div class="row form-group">
-                                        <div class="col-md-12">
-                                            <span class="pagelet_title">People</span>
-                                        </div>
-                                    </div>
-                                    <div class="row form-group">
-                                        <div class="col-md-12">
-                                            <div id="type_ahead_user">
-                                                <div id="dropdown_design_user">
-                                                    <div class="row margin_style">
-                                                        <a class= "user_anchor" href="" >
-                                                            <div class="col-md-3 profile-background">
-                                                                <div class="" >                                                                    
-                                                                    <img  alt="" src="" class="user_image img-responsive profile-photo" onError="this.style.display = 'none'; this.parentNode.className='profile-background'; this.parentNode.getElementsByTagName('div')[0].style.visibility='visible'; "/>
-                                                                    <div class="signature_id" style="visibility:hidden;height:0px"></div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-md-9 font_style">
-                                                                <div class="user_name">
-                                                                </div>
-                                                                <div class="home_town">
-                                                                </div>
-                                                                <div class="country_name">
-                                                                </div>
-                                                            </div>
-                                                        </a> 
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+<div class="row" id="page_late_id" style="display: none; position: absolute; z-index: 1001;">;
+    <div class="col-xs- 10 col-sm-10 col-md-10 col-lg-10">
+        <div class="pagelet">
+            <div class="row">
+                <div class="col-md-4">
+                    <div id="users_temp">
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <span class="pagelet_title">People</span>
                             </div>
-                            <div class="col-md-4">
-                                <div id="news_temp">
-                                    <div class="row form-group">
-                                        <div class="col-md-12">
-                                            <span class="pagelet_title">News</span>
-                                        </div>
-                                    </div>
-                                    <div class="row form-group">
-                                        <div class="col-md-12">
-                                            <div id="type_ahead_news">
-                                                <div id="dropdown_design_news">
-                                                    <div class="row margin_style">
-                                                        <a class= "news_anchor" href="" >
-                                                            <div class="col-md-3">
-                                                                <img src="" class="img-responsive news_image" style="display: none"/>
-                                                            </div>
-                                                            <div class="col-md-9 font_style">
-                                                                <div class="news_title">
-                                                                </div>
-                                                            </div>
-                                                        </a>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <div id="type_ahead_user">
+                                    <div id="dropdown_design_user">
+                                        <div class="row margin_style">
+                                            <a class= "user_anchor" href="" >
+                                                <div class="col-md-3 ">
+                                                    <div class="profile-background" >                                                                    
+                                                        <img  alt="" src="" class="user_image img-responsive profile-photo" onError="this.style.display = 'none'; this.parentNode.className='profile-background'; this.parentNode.getElementsByTagName('div')[0].style.visibility='visible'; "/>
+                                                        <div class="signature_id" style="visibility:hidden;height:0px"></div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div id="recipes_temp">
-                                    <div class="row form-group">
-                                        <div class="col-md-12">
-                                            <span class="pagelet_title">Recipes</span>
-                                        </div>
-                                    </div>
-                                    <div class="row form-group">
-                                        <div class="col-md-12">
-                                            <div id="type_ahead_recipes">
-                                                <div id="dropdown_design_recipes">
-                                                    <div class="row margin_style">
-                                                        <a class= "recipes_anchor" href="" >
-                                                            <div class="col-md-3">
-                                                                <img src="" class="img-responsive recipes_image" style="display: none"/>
-                                                            </div>
-                                                            <div class="col-md-9 font_style">
-                                                                <div class="recipes_title">
-                                                                </div>
-                                                            </div>
-                                                        </a>
+                                                <div class="col-md-9 font_style">
+                                                    <div class="user_name">
+                                                    </div>
+                                                    <div class="home_town">
+                                                    </div>
+                                                    <div class="country_name">
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </a> 
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div id="b_users_temp">
-                                    <div class="row form-group">
-                                        <div class="col-md-12">
-                                            <span class="pagelet_title">Business Groups</span>
-                                        </div>
-                                    </div>
-                                    <div class="row form-group">
-                                        <div class="col-md-12">
-                                            <div id="type_ahead_b_users">
-                                                <div id="dropdown_design_b_users">
-                                                    <div class="row margin_style">
-                                                        <a class= "b_user_anchor" href="" >
-                                                            <div class="col-md-3 profile-background">
-                                                                <div class="" >                                                                    
-                                                                    <img  alt="" src="" class="b_users_image img-responsive profile-photo" onError="this.style.display = 'none'; this.parentNode.className='profile-background'; this.parentNode.getElementsByTagName('div')[0].style.visibility='visible'; "/>
-                                                                    <div class="b_signature_id" style="visibility:hidden;height:0px"></div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-md-9 font_style">
-                                                                <div class="business_name">
-                                                                </div>
-                                                            </div>
-                                                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div id="news_temp">
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <span class="pagelet_title">News</span>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <div id="type_ahead_news">
+                                    <div id="dropdown_design_news">
+                                        <div class="row margin_style">
+                                            <a class= "news_anchor" href="" >
+                                                <div class="col-md-3">
+                                                    <img src="" class="img-responsive news_image" style="display: none"/>
+                                                </div>
+                                                <div class="col-md-9 font_style">
+                                                    <div class="news_title">
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-4">
-                                <div id="blogs_temp">
-                                    <div class="row form-group">
-                                        <div class="col-md-12">
-                                            <span class="pagelet_title">Blogs</span>
-                                        </div>
-                                    </div>
-                                    <div class="row form-group">
-                                        <div class="col-md-12">
-                                            <div id="type_ahead_blogs">
-                                                <div id="dropdown_design_blogs">
-                                                    <div class="row margin_style">
-                                                        <a class= "blogs_anchor" href="" >
-                                                            <div class="col-md-3">
-                                                                <img src="" class="img-responsive blogs_image" style="display: none"/>
-                                                            </div>
-                                                            <div class="col-md-9 font_style">
-                                                                <div class="blogs_title">
-                                                                </div>
-                                                            </div>
-                                                        </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div id="recipes_temp">
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <span class="pagelet_title">Recipes</span>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <div id="type_ahead_recipes">
+                                    <div id="dropdown_design_recipes">
+                                        <div class="row margin_style">
+                                            <a class= "recipes_anchor" href="" >
+                                                <div class="col-md-3">
+                                                    <img src="" class="img-responsive recipes_image" style="display: none"/>
+                                                </div>
+                                                <div class="col-md-9 font_style">
+                                                    <div class="recipes_title">
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-4"></div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div id="b_users_temp">
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <span class="pagelet_title">Business Groups</span>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <div id="type_ahead_b_users">
+                                    <div id="dropdown_design_b_users">
+                                        <div class="row margin_style">
+                                            <a class= "b_user_anchor" href="" >
+                                                <div class="col-md-3">
+                                                    <div class="" >                                                                    
+                                                        <img  alt="" src="" class="b_users_image img-responsive profile-photo" onError="this.style.display = 'none'; this.parentNode.className='profile-background'; this.parentNode.getElementsByTagName('div')[0].style.visibility='visible'; "/>
+                                                        <div class="b_signature_id" style="visibility:hidden;height:0px"></div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-9 font_style">
+                                                    <div class="business_name">
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div id="blogs_temp">
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <span class="pagelet_title">Blogs</span>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <div id="type_ahead_blogs">
+                                    <div id="dropdown_design_blogs">
+                                        <div class="row margin_style">
+                                            <a class= "blogs_anchor" href="" >
+                                                <div class="col-md-3">
+                                                    <img src="" class="img-responsive blogs_image" style="display: none"/>
+                                                </div>
+                                                <div class="col-md-9 font_style">
+                                                    <div class="blogs_title">
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+    </div>
+</div>
