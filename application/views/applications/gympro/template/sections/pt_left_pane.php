6e08af981c2d982d69cc194d831da32383f1ba45
nazmulhasan <nazmul@apurbatech.com>
2015-07-03 23:43:48 +0600

    pt pro left panel is updated

--- application/views/applications/gympro/template/sections/pt_left_pane.php
+++ application/views/applications/gympro/template/sections/pt_left_pane.php
@@ -13,12 +13,12 @@
     </div>
     <div class="ln_wide_portion">
         <div class="ln_item content_text" >
-            <img class="img-responsive" src="<?php echo base_url() ?>resources/images/applications/gympro/programmes.png">
-            <a href="<?php echo base_url().'applications/gympro/programs'?>">Programmes</a>
-        </div>
-        <div class="ln_item content_text" >
             <img class="img-responsive" src="<?php echo base_url() ?>resources/images/applications/gympro/earnings.png">
             <a href="<?php echo base_url()?>applications/gympro/manage_assessments">Assesments</a>
+        </div>
+        <div class="ln_item content_text" >
+            <img class="img-responsive" src="<?php echo base_url() ?>resources/images/applications/gympro/programmes.png">
+            <a href="<?php echo base_url().'applications/gympro/programs'?>">Programmes</a>
         </div>
         <div class="ln_item content_text" >
             <img class="img-responsive" src="<?php echo base_url() ?>resources/images/applications/gympro/missions.png">
