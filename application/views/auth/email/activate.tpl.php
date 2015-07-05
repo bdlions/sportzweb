08b8fc3b96a6fdd8e3d92792d839b01eed1d2f36
nazmulhasan <nazmul@apurbatech.com>
2015-07-06 00:13:08 +0600

    Sonuto registration message and email text are updated

--- application/views/auth/email/activate.tpl.php
+++ application/views/auth/email/activate.tpl.php
@@ -1,6 +1,7 @@
 <html>
 <body>
-	<h1><?php echo sprintf(lang('email_activate_heading'), $identity);?></h1>
-	<p><?php echo sprintf(lang('email_activate_subheading'), anchor('auth/activate/'. $id .'/'. $activation, lang('email_activate_link')));?></p>
+    <p><b>Hello <?php echo $first_name?> <?php echo $last_name?></b></p>
+    <h1><?php echo sprintf(lang('email_activate_heading'), $identity);?></h1>
+    <p><?php echo sprintf(lang('email_activate_subheading'), anchor('auth/activate/'. $id .'/'. $activation, lang('email_activate_link')));?></p>
 </body>
 </html>
--- application/views/display_message.php
+++ application/views/display_message.php
@@ -1,3 +1,9 @@
-<?php 
-    echo lang('account_creation_successful');
-?>
+<div class="row" style="font-size:16px">
+    <?php echo lang('account_creation_check_email_title'); ?>
+</div>
+<div class="row">
+    <?php echo lang('account_creation_successful'); ?>
+</div>
+<div class="row">
+    <?php echo lang('account_creation_check_junk_email'); ?>
+</div>
