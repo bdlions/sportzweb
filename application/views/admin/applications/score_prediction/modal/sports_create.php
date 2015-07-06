3a3c3a0a2210b923ef142a20e6275e4ad45396c2
Redoy <nazmulislamredoy@gmail.com>
2015-07-06 14:40:00 +0600

    sports create modal modify

--- application/views/admin/applications/score_prediction/modal/sports_create.php
+++ application/views/admin/applications/score_prediction/modal/sports_create.php
@@ -1,11 +1,11 @@
 <script type="text/javascript">
-    $(function() {
-        $("#button_create_sports").on("click", function() {
+    $(function () {
+        $("#button_create_sports").on("click", function () {
             if ($("#input_sports_create_title").val().length == 0)
             {
                 //alert("Please assign sports name");
                 var message = "Please assign sports name";
-                    print_common_message(message);
+                print_common_message(message);
                 return;
             }
             $.ajax({
@@ -15,9 +15,9 @@
                 data: {
                     title: $("#input_sports_create_title").val()
                 },
-                success: function(data) {
-                  //  alert(data['message']);
-                   var message = data['message'];
+                success: function (data) {
+                    //  alert(data['message']);
+                    var message = data['message'];
                     print_common_message(message);
                     $("#modal_sports_create").modal('hide');
                     window.location.reload();
@@ -37,26 +37,67 @@
                 <h4 class="modal-title" id="myModalLabel">Create Sports</h4>
             </div>
             <div class="modal-body">
+                <div class="row form-group">
+                    <div class ="col-sm-2"></div>
+                    <label class="col-sm-3 control-label">Sports Name:</label>
+                    <div class ="col-sm-4">
+                        <input id="input_sports_create_title" name="input_sports_create_title" value="" type="text" class="form-control"/>
+                        <input id="input_sports_id" name="input_sports_id" value="" type="hidden" class="form-control"/>
+                    </div>
+                </div>
                 <div class="row">
-                    <div class="row form-group">
-                        <div class ="col-sm-2"></div>
-                        <label class="col-sm-3 control-label">Sports Name:</label>
-                        <div class ="col-sm-4">
-                            <input id="input_sports_create_title" name="input_sports_create_title" value="" type="text" class="form-control"/>
-                            <input id="input_sports_id" name="input_sports_id" value="" type="hidden" class="form-control"/>
-                        </div>
+                    <div class="col-sm-offset-5 col-sm-4">
+                        <input type="checkbox"> 
+                        <span style="vertical-align: top; font-size: 14px;">Position</span>
                     </div>
-                    <div class="row form-group">
-                        <div class ="col-sm-6"></div>
-                        <div class ="col-sm-3">
-                            <button id="button_create_sports" name="button_create_sports" value="" class="form-control btn button-custom pull-right">Create</button>
-                        </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-offset-5 col-sm-4">
+                        <input type="checkbox"> 
+                        <span style="vertical-align: top; font-size: 14px;">Drivers</span>
                     </div>
-                </div>                
-            </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-offset-5 col-sm-4">
+                        <input type="checkbox"> 
+                        <span style="vertical-align: top; font-size: 14px;">Players</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-offset-5 col-sm-4">
+                        <input type="checkbox"> 
+                        <span style="vertical-align: top; font-size: 14px;">Teams</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-offset-5 col-sm-4">
+                        <input type="checkbox"> 
+                        <span style="vertical-align: top; font-size: 14px;">Played</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-offset-5 col-sm-4">
+                        <input type="checkbox"> 
+                        <span style="vertical-align: top; font-size: 14px;">Goal Difference</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-offset-5 col-sm-4">
+                        <input type="checkbox"> 
+                        <span style="vertical-align: top; font-size: 14px;">Points</span>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class ="col-sm-8"></div>
+                    <div class ="col-sm-2">
+                        <button id="button_create_sports" name="button_create_sports" value="" class="form-control btn button-custom pull-right">Create</button>
+                    </div>
+                </div>
+            </div> 
             <div class="modal-footer">
                 <button type="button" class="btn button-custom" data-dismiss="modal">Close</button>
             </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+        </div>
+
+    </div>
+</div>
