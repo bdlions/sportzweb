0cd05515d2a7395dd587e9dd0753eaa8de646cd3
Redoy <nazmulislamredoy@gmail.com>
2015-07-06 13:30:41 +0600

    twitter bg (partial)

--- application/views/nonmember/templates/main.php
+++ application/views/nonmember/templates/main.php
@@ -1,57 +1,57 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>resources/bootstrap3/css/home.css">
 <script type="text/javascript" src="http://www.google.com/recaptcha/api/js/recaptcha_ajax.js"></script>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>resources/bootstrap3/css/common_styles.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>resources/bootstrap3/css/common_styles.css">
 <script type="text/javascript">
-    $(document).ready(function() {
-        $('#next_btn').on('click', function() {
+    $(document).ready(function () {
+        $('#next_btn').on('click', function () {
             if ($("#registration-form").valid() === true) {
                 $("#login_box").hide();
                 $("#left_part").remove();
                 $('#step2').removeAttr("class").show(300);
                 $("#step1").hide(300);
-                Recaptcha.create('<?php echo $public_key?>',
-                    "captcha_div",
-                    {
-                      theme: "clean",
-                      callback: Recaptcha.focus_response_field
-                    }
+                Recaptcha.create('<?php echo $public_key ?>',
+                        "captcha_div",
+                        {
+                            theme: "clean",
+                            callback: Recaptcha.focus_response_field
+                        }
                 );
             }
             return false;
         });
-        
-        $("#r_captcha_input").on("keypress", function(e){
-            if(e.which === 13){
+
+        $("#r_captcha_input").on("keypress", function (e) {
+            if (e.which === 13) {
                 e.preventDefault();
                 $('#register_btn').trigger("click");
             }
         });
-        
-        $('#register_btn').on('click', function(event) {
+
+        $('#register_btn').on('click', function (event) {
             $.ajax({
-                    type: 'POST',
-                    url: '<?php echo base_url() ?>auth/captcha_check_client',
-                    dataType: 'json',
-                    data:$("#registration-form").serialize(),
-                    success: function(data) {
-                        if(data.is_valid){
-                            $("#registration-form").submit();
-                        }
-                        else{
-                            $("#message").text('<?php echo lang('captcha_error_msg')?>').attr('class', "error-message");
-                            Recaptcha.create('<?php echo $public_key?>',
+                type: 'POST',
+                url: '<?php echo base_url() ?>auth/captcha_check_client',
+                dataType: 'json',
+                data: $("#registration-form").serialize(),
+                success: function (data) {
+                    if (data.is_valid) {
+                        $("#registration-form").submit();
+                    }
+                    else {
+                        $("#message").text('<?php echo lang('captcha_error_msg') ?>').attr('class', "error-message");
+                        Recaptcha.create('<?php echo $public_key ?>',
                                 "captcha_div",
                                 {
-                                  theme: "clean",
-                                  callback: Recaptcha.focus_response_field
+                                    theme: "clean",
+                                    callback: Recaptcha.focus_response_field
                                 }
-                            );
-                        }
+                        );
                     }
-                });
-                return false;
-         });
-		 
+                }
+            });
+            return false;
+        });
+
         // Setup form validation on the #register-form element
         $("#registration-form").validate({
             // Specify the validation rules
@@ -103,54 +103,83 @@
 
             }
         });
-    });    
+    });
 </script>
-<div class="row main">
-    <div class="col-md-7 col-md-offset-1" id="left_part">
-        <img src="<?php echo base_url().LOGIN_PAGE_IMAGE_PATH.$current_configuration['img'] ?>" class="img-responsive" style="padding-top:15px">
-        <div class="hp_left_img_title"><?php echo $current_configuration['img_description']?></div>  
-    </div>
-    <div class="col-md-4" id="right_part">
-        <?php echo form_open("auth/login", array('id' => 'registration-form', 'role' => 'form')); ?>
-        <div id="step1">
-            <div class="home-left-tit">Sign Up</div>
-            <div class="form-group">
-                <?php echo form_input($r_first_name + array('class' => 'form-control')); ?>
-            </div>
-            <div class="form-group">
-                <?php echo form_input($r_last_name + array('class' => 'form-control')); ?>
-            </div>
-            <div class="form-group">
-                <?php echo form_input($r_email + array('class' => 'form-control')); ?>
-            </div>
-            <div class="form-group">
-                <?php echo form_input($r_email_conf + array('class' => 'form-control')); ?>
-            </div>
-            <div class="form-group">
-                <?php echo form_input($r_password + array('class' => 'form-control')); ?>
-            </div>
-            <div class="form-group pull-right">
-                <?php echo form_input($next_btn + array('class' => 'btn button-custom')); ?>
-            </div>
-            <div class="form-group business_profile_ancor">
-                <?php echo anchor("business_profile", "Create a profile for your  business."); ?>
+<div>
+    <ul class="cb-slideshow bg_sliding_ul">
+        <li>
+            <span>Image 01</span>
+        </li>
+        <li>
+            <span>Image 02</span>
+        </li>
+        <li>
+            <span>Image 03</span>
+        </li>
+        <li>
+            <span>Image 04</span>
+        </li>
+        <li>
+            <span>Image 05</span>
+        </li>
+        <li>
+            <span>Image 06</span>
+        </li>
+    </ul>
+    <div class="container-fluid" style="margin-left: -15px; margin-right: -15px;">
+        <div class="row padding_over_row">
+            <div class="col-md-offset-1 col-md-10">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <span style="color: #fff;">A social hub that allows you to connect and share health, sports and fitness information with people.
+                                    Members have access to a variety of health, sports and fitness applications including diets, personal
+                                    trainers, blogs, food recipes and news.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                            <div class="pagelet_auth">
+                                <div class="row form-group">
+                                    <div class="col-md-12">
+                                        <span style="color: #89c1f4; font-size: 27px;">Sign Up</span>
+                                    </div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                        <input class="form-control" type="text" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                        <input class="form-control" type="text" placeholder="Confirm Email">
+                                    </div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                        <input class="form-control" type="password" placeholder="Passowrd">
+                                    </div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                        <button class="btn button-custom pull-right">Join Now</button>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                        <div style="border-bottom: 1px solid #cecece;"></div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                        <a href="" class="business_profile_achor_auth">Create a profile for your business.</a>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div id="step2" class="security-captcha">
-            <div class="home-left-tit">
-                <div class="home-left-title home-left-title2">Sign Up</div>
-            </div>
-            <div class="heading_medium_thin"><?php echo lang('security_check_label'); ?></div>
-            <div class="row form-group"></div> 
-            <div class="form-group">
-                <?php echo lang('please_enter_the_characters_below_label'); ?>
-            </div> 
-            <div class="form-group" id="captcha_div"></div>
-            <div class="form-group">
-                <?php echo form_input($register_btn + array('class' => 'btn button-custom')); ?>
-            </div>
-        </div>
-        <div id="message"></div>
-        <?php echo form_close(); ?>
     </div>
 </div>
