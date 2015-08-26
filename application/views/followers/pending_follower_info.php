e24a02e7a6883799adf25f579fc85e52773b9a62
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2015-08-26 17:04:36 +0600

    member followers bug fixing

--- application/views/followers/pending_follower_info.php
+++ application/views/followers/pending_follower_info.php
@@ -1,48 +1,49 @@
-<div class="col-xs-5">
+<div class="col-md-5 form-group">
     <div style="border: 1px solid lightgray; padding: 10px">
-    
-    <div class="row">
-        <div class="col-xs-3">
-            <a href='<?php echo base_url() . "member_profile/show/{$follower->user_id}" ?>' class="profile-name"> 
-                <div>
-                    <img alt="<?php echo $follower->first_name[0] . $follower->last_name[0] ?>" src="<?php echo base_url() . PROFILE_PICTURE_DISPLAY_PATH . $follower->photo ?>" class="img-responsive profile-photo" onError="this.style.display = 'none'; this.parentNode.className='profile-background'; this.parentNode.getElementsByTagName('div')[0].style.visibility='visible'; " /> 
-                    <div style="visibility:hidden; height: 0;"><?php echo $follower->first_name[0].$follower->last_name[0] ?></div>
-                </div>
-            </a>
-        </div>
-        <div class="col-xs-6">
-            <div class="row heading_medium_thin">
-                <div class="col-md-12"><?php echo $follower->first_name . " ". $follower->last_name?></div>
-            </div>
-            <div class="row small_text_pale">
-                <div class="col-md-12"><?php $follower_count = count(json_decode($follower->relations)); echo $follower_count == 1? "{$follower_count} Follower": "{$follower_count} Followers"?></div>
-            </div>
-            <div class="row small_text_pale">
-                <div class="col-md-12"><?php echo $follower->about_me ?></div>
-            </div>
-        </div>
-        
-        <div class="col-xs-3">
-            <div class="dropdown friends-satus-dropdown pull-right">
-                <a id="friends_status" data-toggle="dropdown" href="#" >
-                    <img src="<?php echo base_url() ?>resources/images/friends_status.png" alt=""/>
-                    <span class="caret"></span>
+
+        <div class="row">
+            <div class="col-md-3">
+                <a href='<?php echo base_url() . "member_profile/show/{$follower->user_id}" ?>' class="profile-name"> 
+                    <div>
+                        <img alt="<?php echo $follower->first_name[0] . $follower->last_name[0] ?>" src="<?php echo base_url() . PROFILE_PICTURE_DISPLAY_PATH . $follower->photo ?>" class="img-responsive profile-photo" onError="this.style.display = 'none'; this.parentNode.className='profile-background'; this.parentNode.getElementsByTagName('div')[0].style.visibility='visible'; " /> 
+                        <div style="visibility:hidden; height: 0;"><?php echo $follower->first_name[0] . $follower->last_name[0] ?></div>
+                    </div>
                 </a>
-                <ul class="dropdown-menu" role="menu" aria-labelledby="friends_status">
-                    <li role="presentation">
-                        <a role="menuitem" tabindex="-1" href="javascript:void(0)" onclick="open_modal_accept_confirm('<?php echo $follower->user_id; ?>')">Accept Request</a>
-                    </li>
-                    <li role="presentation">
-                        <a role="menuitem" tabindex="-1" href="javascript:void(0)" onclick="open_modal_block_confirm('<?php echo $follower->user_id; ?>')">Block</a>
-                    </li>
-                    <li role="presentation">
-                        <a role="menuitem" tabindex="-1" href="javascript:void(0)" onclick="open_modal_report('<?php echo $follower->user_id; ?>')">Report</a>
-                    </li>
-                </ul>
             </div>
-       </div>
-    </div>
+            <div class="col-md-6">
+                <div class="row heading_medium_thin">
+                    <div class="col-md-12"><?php echo $follower->first_name . " " . $follower->last_name ?></div>
+                </div>
+                <div class="row small_text_pale">
+                    <div class="col-md-12"><?php $follower_count = count(json_decode($follower->relations));
+echo $follower_count == 1 ? "{$follower_count} Follower" : "{$follower_count} Followers" ?></div>
+                </div>
+                <div class="row small_text_pale">
+                    <div class="col-md-12"><?php echo $follower->about_me ?></div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="dropdown friends-satus-dropdown pull-right">
+                    <a id="friends_status" data-toggle="dropdown" href="#" >
+                        <img src="<?php echo base_url() ?>resources/images/friends_status.png" alt=""/>
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" role="menu" aria-labelledby="friends_status">
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="javascript:void(0)" onclick="open_modal_accept_confirm('<?php echo $follower->user_id; ?>')">Accept Request</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="javascript:void(0)" onclick="open_modal_block_confirm('<?php echo $follower->user_id; ?>')">Block</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="javascript:void(0)" onclick="open_modal_report('<?php echo $follower->user_id; ?>')">Report</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
-<div class="col-xs-1">    
+<div class="col-md-1">    
 </div>
--- application/views/followers/template/section/member_header.php
+++ application/views/followers/template/section/member_header.php
@@ -1,33 +1,33 @@
 <script type="text/javascript">
-    $(function(){
+    $(function() {
         $("#search_followers_box").typeahead([{
-                name:"show_followers",
-                prefetch:{
-                            url: '<?php echo base_url()?>search/get_followers/',
-                            ttl: 0
-                        },
+                name: "show_followers",
+                prefetch: {
+                    url: '<?php echo base_url() ?>search/get_followers/',
+                    ttl: 0
+                },
                 template: [
-                    '<div class="row">'+
-                        '<div class="col-md-3">'+
-                            '<div>'+
-                                '<img alt="{{signature}}" src="<?php echo base_url().PROFILE_PICTURE_DISPLAY_PATH?>{{photo}}" class="img-responsive profile-photo" onError="this.style.display = \'none\'; this.parentNode.className=\'profile-background profile_background_search_bar\'; this.parentNode.getElementsByTagName(\'div\')[0].style.visibility=\'visible\'; "/>'+
-                                '<div style="visibility:hidden;height:0px">{{signature}}</div>'+
-                            '</div>'+
-                        '</div>'+
-                        '<div class="col-md-9">'+
-                            '<div class="row col-md-12 profile-name">'+
-                                '{{first_name}} {{last_name}}'+
-                            '</div>'+
-                        '</div>'+
-                    '</div>'
-                  ].join(''),
+                    '<div class="row">' +
+                            '<div class="col-md-3">' +
+                            '<div>' +
+                            '<img alt="{{signature}}" src="<?php echo base_url() . PROFILE_PICTURE_DISPLAY_PATH ?>{{photo}}" class="img-responsive profile-photo" onError="this.style.display = \'none\'; this.parentNode.className=\'profile-background profile_background_search_bar\'; this.parentNode.getElementsByTagName(\'div\')[0].style.visibility=\'visible\'; "/>' +
+                            '<div style="visibility:hidden;height:0px">{{signature}}</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '<div class="col-md-9">' +
+                            '<div class="row col-md-12 profile-name">' +
+                            '{{first_name}} {{last_name}}' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>'
+                ].join(''),
                 engine: Hogan
-        }]).on('typeahead:selected', function (obj, datum) {
-                window.location = "<?php echo base_url()?>member_profile/show/" + datum.user_id;
-            });
+            }]).on('typeahead:selected', function(obj, datum) {
+            window.location = "<?php echo base_url() ?>member_profile/show/" + datum.user_id;
+        });
     });
-    
-    function open_modal_unfollow_confirm(follower_id){
+
+    function open_modal_unfollow_confirm(follower_id) {
         $.ajax({
             dataType: 'json',
             type: "POST",
@@ -36,17 +36,17 @@
                 follower_id: follower_id
             },
             success: function(data) {
-                $("#div_unfollow_confirm_follower_info").html(tmpl("tmpl_user_info", data.user_info)); 
-                $('#span_unfollow_confirm_message').text('Unfollow '+data.user_info.first_name+' '+data.user_info.last_name+'?');
+                $("#div_unfollow_confirm_follower_info").html(tmpl("tmpl_user_info", data.user_info));
+                $('#span_unfollow_confirm_message').text('Unfollow ' + data.user_info.first_name + ' ' + data.user_info.last_name + '?');
                 $('#follower_id_confirm_unfollow').val(follower_id);
                 $('#modal_unfollow_confirm').modal('show');
             }
-        });        
+        });
         //$('#follower_id_confirm_unfollow').val(follower_id);
         //$('#modal_unfollow_confirm').modal('show');
     }
-    
-    function open_modal_block_confirm(follower_id){
+
+    function open_modal_block_confirm(follower_id) {
         $.ajax({
             dataType: 'json',
             type: "POST",
@@ -55,18 +55,18 @@
                 follower_id: follower_id
             },
             success: function(data) {
-                $("#div_block_confirm_follower_info").html(tmpl("tmpl_user_info", data.user_info)); 
-                $('#span_block_confirm_message').text('Block '+data.user_info.first_name+' '+data.user_info.last_name+'?');
+                $("#div_block_confirm_follower_info").html(tmpl("tmpl_user_info", data.user_info));
+                $('#span_block_confirm_message').text('Block ' + data.user_info.first_name + ' ' + data.user_info.last_name + '?');
                 $('#follower_id_confirm_block').val(follower_id);
                 $('#modal_block_confirm').modal('show');
             }
         });
-        
+
         //$('#follower_id_confirm_block').val(follower_id);
         //$('#modal_block_confirm').modal('show');
     }
-    
-    function open_modal_unblock_confirm(follower_id){
+
+    function open_modal_unblock_confirm(follower_id) {
         $.ajax({
             dataType: 'json',
             type: "POST",
@@ -75,20 +75,20 @@
                 follower_id: follower_id
             },
             success: function(data) {
-                $("#div_unblock_confirm_follower_info").html(tmpl("tmpl_user_info", data.user_info)); 
-                $('#span_unblock_confirm_message').text('Unblock '+data.user_info.first_name+' '+data.user_info.last_name+'?');
+                $("#div_unblock_confirm_follower_info").html(tmpl("tmpl_user_info", data.user_info));
+                $('#span_unblock_confirm_message').text('Unblock ' + data.user_info.first_name + ' ' + data.user_info.last_name + '?');
                 $('#follower_id_confirm_unblock').val(follower_id);
                 $('#modal_unblock_confirm').modal('show');
             }
-        });        
+        });
         //$('#follower_id_confirm_unblock').val(follower_id);
         //$('#modal_unblock_confirm').modal('show');
     }
-    
-    function open_modal_report(follower_id){
+
+    function open_modal_report(follower_id) {
         $('#follower_id').val(follower_id);
-        $('#<?php echo FOLLOWER_REPORT_TYPE_SHARED_CONTENT_ID?>').attr('checked', false);
-        $('#<?php echo FOLLOWER_REPORT_TYPE_ACCOUNT_ID?>').attr('checked', false);
+        $('#<?php echo FOLLOWER_REPORT_TYPE_SHARED_CONTENT_ID ?>').attr('checked', false);
+        $('#<?php echo FOLLOWER_REPORT_TYPE_ACCOUNT_ID ?>').attr('checked', false);
         $.ajax({
             dataType: 'json',
             type: "POST",
@@ -97,8 +97,8 @@
                 follower_id: follower_id
             },
             success: function(data) {
-                $("#div_follower_info").html(tmpl("tmpl_user_info", data.user_info)); 
-                $('#span_shared_content').text('Report content shared by '+data.user_info.first_name+' '+data.user_info.last_name);
+                $("#div_follower_info").html(tmpl("tmpl_user_info", data.user_info));
+                $('#span_shared_content').text('Report content shared by ' + data.user_info.first_name + ' ' + data.user_info.last_name);
                 $('#modal_report_content').modal('show');
             }
         });
@@ -112,24 +112,27 @@
             <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
         </div>
     </div>       
-    <div class="col-md-8">            
-        <div class="col-md-3">
-            <?php if($follow_permission->value == FOLLOWER_ACCEPTANCE_TYPE_MANUAL){?>
-                <a href="<?php echo base_url().'followers/pending_followers'?>"><button style="width:100%" class="btn button-custom" id="button_pending_request">Pending Requests</button></a>
-            <?php }?>
-        </div>
-        <div class="col-md-2">
-            <a href="<?php echo base_url().'followers/invite'?>"><button style="width:100%" class="btn button-custom">Invite</button></a>                    
-        </div>
-        <div class="col-md-3">
-            <a href="<?php echo base_url().'followers/show/'.$user_id?>"><button style="width:100%" class="btn button-custom">My Followers</button></a>
-        </div>
-        <div class="col-md-3">
-            <a href="<?php echo base_url().'followers/blocked_followers/'.$user_id?>"><button style="width:100%" class="btn button-custom">Blocked Members</button></a>                    
-        </div>
+    <div class="col-md-8"> 
+        <ul class="list-inline">
+            <li class="form-group">
+                <?php if ($follow_permission->value == FOLLOWER_ACCEPTANCE_TYPE_MANUAL) { ?>
+                    <a href="<?php //echo base_url().'followers/pending_followers'    ?>"><button style="width:100%" class="btn button-custom btn-sm" id="button_pending_request">Pending Requests</button></a>
+                <?php } ?>
+            </li>
+            <li class="form-group">
+                <a href="<?php echo base_url() . 'followers/invite' ?>"><button style="width:100%" class="btn button-custom btn-sm">Invite</button></a>                    
+            </li>
+            <li class="form-group">
+                <a href="<?php echo base_url() . 'followers/show/' . $user_id ?>"><button style="width:100%" class="btn button-custom btn-sm">My Followers</button></a>
+            </li>
+            <li class="form-group">
+                <a href="<?php echo base_url() . 'followers/blocked_followers/' . $user_id ?>"><button style="width:100%" class="btn button-custom btn-sm">Blocked Members</button></a>                    
+            </li>
+        </ul>
     </div>
 </div>
-<?php $this->load->view("followers/modal_report");
-      $this->load->view("followers/modal_unfollow_confirm");
-      $this->load->view("followers/modal_block_confirm");
-      $this->load->view("followers/modal_unblock_confirm");
+<?php
+$this->load->view("followers/modal_report");
+$this->load->view("followers/modal_unfollow_confirm");
+$this->load->view("followers/modal_block_confirm");
+$this->load->view("followers/modal_unblock_confirm");
