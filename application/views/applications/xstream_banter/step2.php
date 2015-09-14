145731dfacbefa9be160124b315b1dd1bbd05061
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2015-09-14 14:33:13 +0600

    xstream banter step1 & step2 with css bug fixing

--- application/views/applications/xstream_banter/step2.php
+++ application/views/applications/xstream_banter/step2.php
@@ -6,11 +6,11 @@
     });
 </script>
 <div class="col-md-9 column xb-home">
-    <div class="row col-md-6 col-md-offset-3">
+    <div class="row col-md-6 col-md-offset-5">
         <div class="row" style="padding-bottom:150px"></div>
         <a href="<?php echo base_url().'applications/xstream_banter'; ?>" ><img src="<?php echo base_url() ?>resources/images/xb_logo.png" /></a>
     </div>
-    <div class="row col-md-6 col-md-offset-3 form-horizontal">        
+    <div class="row col-md-6 col-md-offset-5 form-horizontal">        
         <?php echo form_open("applications/xstream_banter/step2/".$sports_id, array('id' => 'form_select_tournament', 'class' => 'form-horizontal')); ?>
         <div class="form-group">
             <div class ="col-md-12">
